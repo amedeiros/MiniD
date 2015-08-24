4e9919d51a12291510a992e69d8fed591d642b19
amedeiros <andrew@amedeiros.com>
2015-08-24 15:50:40 -0400

    some updates

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # MiniD
 MiniD is a minimal IRC server written completely in java 8
+
+The idea behind this was to have a simple place to tests IRC bots locally.
--- src/main/java/minid/MiniD.java
+++ src/main/java/minid/MiniD.java
@@ -8,16 +8,16 @@ import org.apache.commons.cli.CommandLine;
  * Created by Andrew Medeiros on 4/30/15.
  */
 public class MiniD {
-    private ServerConfig serverConfig = new ServerConfig();
     private Server server;
 
     public MiniD(CommandLine commandLine) throws Exception {
+        ServerConfig serverConfig = new ServerConfig();
         if (commandLine.hasOption("host"))
             serverConfig.setHost(commandLine.getOptionValue("host"));
         if (commandLine.hasOption("port"))
             serverConfig.setPort(Integer.parseInt(commandLine.getOptionValue("port")));
 
-        server = new Server(serverConfig);
+        this.server = new Server(serverConfig);
     }
 
     public void start() { new Thread(server).start(); }
